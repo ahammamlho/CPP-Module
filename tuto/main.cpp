761fd09ccd84bf938a21048913aeb5d2d0d5e340
Lhoussaine Ahammam <lahammam@e1r5p3.1337.ma>
2022-12-02 15:41:47 +0100

    les conditions et les boucles

--- tuto/main.cpp
+++ tuto/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:23:49 by lahammam          #+#    #+#             */
-/*   Updated: 2022/12/02 14:53:38 by lahammam         ###   ########.fr       */
+/*   Updated: 2022/12/02 15:40:55 by lahammam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,39 @@
 
 int main()
 {
-    std::cout << "----------saisie utilisateur---------" << std::endl;
+    std::cout << "----------conditions---------" << std::endl;
     
-    std::cout << "Tapez votre name : ";
-    std::string name;
-    // std::cin >> name;
-    std::getline(std::cin, name);
-    std::cout << "name saisi est : " << name << std::endl;
-    std::cin.clear(); // .good()  .bad()   .fail()
+    int nbr = 5;
+    bool is_empty = false;
+
+    // if (nbr < 10)
+    //     std::cout << nbr << " < 10" << std::endl;
+    // else
+    //      std::cout << nbr << " < 10" << std::endl;
+    // switch (nbr)
+    // {
+    // case 1:
+    //     std::cout << nbr << " = 1" << std::endl;
+    //     break;
+    // case 2:
+    //     std::cout << nbr << " = 2" << std::endl;
+    //     break;
+    // default:
+    //     std::cout << "!=1 && !=2" << std::endl;
+    //     break;
+    // }
+    int i = 0;
+    while (i <= nbr)
+    {
+        std::cout << "while " << i << std::endl;
+        i++;
+    }
+    for (int i = 0; i < nbr; i++)
+    {
+        std::cout << "for " << i << std::endl;
+    }
     
+        
     std::cout << "--------------------------------" << std::endl;
     return (0);
 }
