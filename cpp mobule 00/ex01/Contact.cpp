4a27e98e0f2f7698a3019f39a8524dcdc284e27c
Lhoussaine Ahammam <lahammam@e1r6p3.1337.ma>
2022-12-05 12:57:55 +0100

    cpp00/ex00

---
+++ cpp mobule 00/ex01/Contact.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 12:05:04 by lahammam          #+#    #+#             */
+/*   Updated: 2022/12/05 12:53:17 by lahammam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+class Contact
+{
+protected:
+    std::string firstName;
+    std::string lastName;
+    std::string nickName;
+    std::string phoneNumber;
+    std::string darketSecret;    
+};
