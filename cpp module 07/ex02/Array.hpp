9cc52e2decbb40bd12fe2266a8f65a6c1ac29539
Lhoussaine Ahammam <lahammam@e1r5p3.1337.ma>
2023-01-10 14:51:24 +0100

    gsd

---
+++ cpp module 07/ex02/Array.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 11:25:52 by lahammam          #+#    #+#             */
+/*   Updated: 2023/01/10 14:50:33 by lahammam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <iostream>
+#include <string.h>
+
+template <class T>
+class Array
+{
+private:
+    T element;
+
+public:
+    Array()
+    {
+        std::array<T, 0> emptyArray;
+    };
+    ~Array();
+};
+
+Array::Array()
+{
+}
+
+Array::~Array()
+{
+}
+
+#endif
