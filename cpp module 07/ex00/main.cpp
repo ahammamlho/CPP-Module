dcbfc1127ce9eba95167cd76270413f7ea3a2cce
Lhoussaine Ahammam <lahammam@e1r5p3.1337.ma>
2023-01-10 14:28:53 +0100

    finish ex00/day07

---
+++ cpp module 07/ex00/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 15:32:55 by lahammam          #+#    #+#             */
+/*   Updated: 2023/01/10 14:18:37 by lahammam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T temp = a;
+
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    if (a < b)
+        return (a);
+    else
+        return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    if (a > b)
+        return (a);
+    else
+        return b;
+}
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+};
