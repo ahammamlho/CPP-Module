239a8fd416db49a1b8fc59e79324a6d813dbc1bf
Lhoussaine Ahammam <lahammam@e1r3p13.1337.ma>
2023-01-26 13:58:49 +0100

    finish day08

--- cpp module 08/ex02/MutantStack.hpp
+++ cpp module 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 11:25:52 by lahammam          #+#    #+#             */
-/*   Updated: 2023/01/25 23:10:27 by lahammam         ###   ########.fr       */
+/*   Updated: 2023/01/26 13:53:35 by lahammam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,20 @@ class MutantStack : public std::stack<T, Container>
 private:
 
 public:
+    typedef typename Container::iterator iterator;
+    
     MutantStack(){};
 
+    typename Container::iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    typename Container::iterator end()
+    {
+        return this->c.end();
+    }
+    
     ~MutantStack(){};
 };
 
--- cpp module 08/ex02/main.cpp
+++ cpp module 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:32:55 by lahammam          #+#    #+#             */
-/*   Updated: 2023/01/25 21:37:39 by lahammam         ###   ########.fr       */
+/*   Updated: 2023/01/26 13:57:57 by lahammam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,28 @@
 
 int main(void)
 {
-    MutantStack<int> mystack;
-
-    mystack.push(888);
-    mystack.push(222);
-    mystack.push(66);
-    mystack.pop();
-
-    std::cout << mystack.top() << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+  
     return 0;
+    
 };
